8018199d3921c59cbe630604c98a8f7fe405945b
Markian Tsypuk <markianzypuk@gmail.com>
2025-11-05 03:44:03 +0100

    Home, Projects, About and Contact Pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html','./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './projects.html', './contact.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: '#F7F3E3',
+        dark: '#0B3954',
+      },
+    },
   },
   plugins: [],
 }
